f00892b3f1273827ccd777586a5227de982536e6
Rohit Singh Sengar <rsengar257@gmail.com>
2026-08-31 12:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ function firstChar(text) {
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-// const text = prompt("Enter text:");
-// alert(firstChar(text));
+const text = prompt("Enter text:");
+alert(firstChar(text));
